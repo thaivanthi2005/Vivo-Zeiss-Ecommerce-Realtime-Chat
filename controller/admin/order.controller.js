5e1b5b4c4f554e541864bfb055fb61a01d3d1e0e
thaivanthi2005 <thaivanthi2005@gmail.com>
2026-09-12 23:33:49 +0700

    feat(admin ) : add backend logic to render order index and detail pages

--- controller/admin/order.controller.js
+++ controller/admin/order.controller.js
@@ -6,7 +6,23 @@ module.exports.index = async (req,res) =>{
     const orders = await Order.find(find);
 
     res.render("admin/pages/orders/index",{
-        pagetitle: "Đơn Hàng",
+    pagetitle: "Đơn Hàng",
     orders: orders,
+    })
+}
+
+module.exports.detail = async (req,res) =>{
+    const orderId = req.params.id;
+
+    const InfoOder = await Order.findOne({_id:orderId});
+
+    if(!InfoOder){
+        res.redirect(`${system_config.prefixAdmin}/orders`);
+        return;
+    }
+
+    res.render("admin/pages/orders/detail",{
+    pagetitle: "Chi Tiết Đơn Hàng",
+    order: InfoOder,
     })
 }
--- routes/admin/order.route.js
+++ routes/admin/order.route.js
@@ -6,7 +6,7 @@ const middleware = require("../../middleware/upload.middleware");
 const controller = require("../../controller/admin/order.controller");
 
 router.get("/",controller.index);
-
+router.get("/detail/:id",controller.detail);
 
 
 module.exports = router;
