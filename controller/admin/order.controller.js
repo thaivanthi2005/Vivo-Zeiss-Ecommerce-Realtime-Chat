16d1fa0fdcae45db7ec498ce6209a7db45eee9c7
thaivanthi2005 <thaivanthi2005@gmail.com>
2026-09-13 23:55:17 +0700

    feat (admin) : connect database and display product data with prices on order detail page

--- controller/admin/order.controller.js
+++ controller/admin/order.controller.js
@@ -1,5 +1,5 @@
 const Order = require("../../models/order.model");
-
+const Product = require("../../models/products.model")
 module.exports.index = async (req,res) =>{
     let find = {};
 
@@ -13,13 +13,27 @@ module.exports.index = async (req,res) =>{
 
 module.exports.detail = async (req,res) =>{
     const orderId = req.params.id;
-
-    const InfoOder = await Order.findOne({_id:orderId});
+let ALLtotalPrice = 0;
+    const InfoOder = await Order.findOne({_id:orderId}).lean();
 
     if(!InfoOder){
         res.redirect(`${system_config.prefixAdmin}/orders`);
         return;
     }
+
+    for (const item of InfoOder.products) {
+        const InfoProduct = await Product.findOne({
+            _id: item.product_id
+        }).select('thumbnail title price discountPercentage -_id').lean();
+        
+        const totalPrice = InfoProduct.price - (InfoProduct.price*InfoProduct.discountPercentage/100);
+        
+        item.totalPrice = totalPrice;
+        item.productInfo = InfoProduct;
+
+        ALLtotalPrice = ALLtotalPrice + totalPrice;
+    }
+    InfoOder.totalPrice = ALLtotalPrice;
 
     res.render("admin/pages/orders/detail",{
     pagetitle: "Chi Tiết Đơn Hàng",
